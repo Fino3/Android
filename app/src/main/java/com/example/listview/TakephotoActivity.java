7a599c5feb4e9451fb0187183d78b1bc18a2fd89
Hermione <1113185900@qq.com>
2021-11-12 01:20:24 +0800

    123

--- app/src/main/java/com/example/listview/TakephotoActivity.java
+++ app/src/main/java/com/example/listview/TakephotoActivity.java
@@ -1,43 +1,218 @@
 package com.example.listview;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+
+import android.os.FileUtils;
 import android.provider.MediaStore;
+import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.View;
+import android.webkit.URLUtil;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 
 public class TakephotoActivity extends AppCompatActivity implements View.OnClickListener {
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    ImageButton ima1;
+    public static final File SDPATH = Environment.getExternalStorageDirectory() ;
+
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.take_photos);
-        findViewById(R.id.img_photo).setOnClickListener(this);
+        ima1=findViewById(R.id.img_photo);
+        ima1.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View view) {
-        if (TakephotoActivity.checkCameraHardware(getApplicationContext())) {
-            dispatchTakePictureIntent();
-        } else {
-            Toast.makeText(getApplicationContext(),"设备不支持",Toast.LENGTH_LONG).show();
+        Intent intent;
+        if (Build.VERSION.SDK_INT >= 23) {
+            int checkPermission = ContextCompat.checkSelfPermission(TakephotoActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE);
+            if (checkPermission != PackageManager.PERMISSION_GRANTED) {
+                (new AlertDialog.Builder(TakephotoActivity.this)).setMessage("您需要在设置里打开存储空间权限。").setPositiveButton("确认", new android.content.DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        ActivityCompat.requestPermissions(TakephotoActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 100);
+                    }
+                }).setNegativeButton("取消", (android.content.DialogInterface.OnClickListener) null).create().show();
+                return;
+            }
+        }
+        intent = new Intent();
+        //MediaStore.ACTION_IMAGE_CAPTURE  调用系统的照相机
+        intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, 0x3);
+
+        /*Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);*/
+    }
+
+
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 0x1) {
+            if (data != null) {
+                Uri uri = data.getData();
+                //裁剪图片
+                getImg(uri);
+            } else {
+                return;
+            }
+        }
+        if (requestCode == 0x2) {
+            if (data != null) {
+                Bundle bundle = data.getExtras();
+                //得到图片
+                Bitmap bitmap = bundle.getParcelable("data");
+//                //保存到图片到本地
+                saveBitmap(bitmap, "abc");
+                //设置图片
+                ima1.setImageBitmap(bitmap);
+            } else {
+                return;
+            }
+        }
+        if (requestCode == 0x3) {
+            if (data != null) {
+                Bundle bundle = data.getExtras();
+                Bitmap bitmap = bundle.getParcelable("data");
+                saveBitmap(bitmap, "abc");
+                ima1.setImageBitmap(bitmap);
+            } else {
+                return;
+            }
         }
     }
 
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+    private void getImg(Uri uri) {
+        try {
+            InputStream inputStream = getContentResolver().openInputStream(uri);
+            //从输入流中解码位图
+            // Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+            //保存位图
+            // img.setImageBitmap(bitmap);
+            cutImg(uri);
+            //关闭流
+            inputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
-    private static  boolean checkCameraHardware(Context context) {
-        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY))
-            return true;
-        return false;
+
+
+    //裁剪图片
+    private void cutImg(Uri uri) {
+        if (uri != null) {
+            Intent intent = new Intent("com.android.camera.action.CROP");
+            intent.setDataAndType(uri, "image/*");
+            //true:出现裁剪的框
+            intent.putExtra("crop", "true");
+            //裁剪宽高时的比例
+            intent.putExtra("aspectX", 1);
+            intent.putExtra("aspectY", 1);
+            //裁剪后的图片的大小
+            intent.putExtra("outputX", 300);
+            intent.putExtra("outputY", 300);
+            intent.putExtra("return-data", true);  // 返回数据
+            intent.putExtra("output", uri);
+            intent.putExtra("scale", true);
+            startActivityForResult(intent, 0x2);
+        } else {
+            return;
+        }
     }
+
+    public static void saveBitmap(Bitmap bm, String picName) {
+        Log.e("", "保存图片");
+        try {
+
+            File f = new File(SDPATH, picName + ".JPEG");
+            if (f.exists()) {
+                f.delete();
+            }
+            FileOutputStream out = new FileOutputStream(f);
+            bm.compress(Bitmap.CompressFormat.JPEG, 90, out);
+            out.flush();
+            out.close();
+            Log.e("", "已经保存");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /*protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+
+            new DateFormat();
+            String name = DateFormat.format("yyyyMMdd_hhmmss", Calendar.getInstance(Locale.CHINA)) + ".jpg";
+            Toast.makeText(this, name, Toast.LENGTH_LONG).show();
+            Bundle bundle = data.getExtras();
+            Bitmap bitmap = (Bitmap) bundle.get("data");// 获取相机返回的数据，并转换为Bitmap图片格式
+
+            FileOutputStream b = null;
+            File file = new File("/Image");
+            file.mkdirs();// 创建文件夹
+            String fileName = "/Image/"+name;
+
+            try {
+                try {
+                    b = new FileOutputStream(fileName);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, b);// 把数据写入文件
+            } finally {
+                try {
+                    try {
+                        b.flush();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    b.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            try
+            {
+                ima1.setImageBitmap(bitmap);// 将图片显示在ImageView里
+            }catch(Exception e)
+            {
+                Log.e("error", e.getMessage());
+            }
+
+        }
+    }*/
+
 }

2975ddd6a57591b18f774a70335523010f04dda2
Fino3 <49306552+Fino3@users.noreply.github.com>
2021-11-11 19:41:59 +0800

    demo

--- app/src/main/java/com/example/listview/TakephotoActivity.java
+++ app/src/main/java/com/example/listview/TakephotoActivity.java
@@ -1,7 +1,13 @@
 package com.example.listview;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class TakephotoActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.take_photos);
 
+    }
 }
--- app/src/main/java/com/example/listview/pho1fragment.java
+++ app/src/main/java/com/example/listview/pho1fragment.java
@@ -1,5 +1,6 @@
 package com.example.listview;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,13 +8,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link pho1fragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class pho1fragment extends Fragment {
+public class pho1fragment extends Fragment implements View.OnClickListener {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,10 +26,12 @@ public class pho1fragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
+    private Button btn_takephotos;
+    private Button btn_history;
+
     public pho1fragment() {
         // Required empty public constructor
     }
-
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
@@ -53,12 +57,23 @@ public class pho1fragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        //findViewById(R.id.btn_takephotos).setOnClickListener(this);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = View.inflate(getActivity(),R.layout.fragment_pho1fragment,null);
+        view.findViewById(R.id.btn_takephotos).setOnClickListener(this);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pho1fragment, container, false);
+        return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent=new Intent();
+        intent.setClass(getActivity().getApplicationContext(),TakephotoActivity.class);
+        startActivity(intent);
     }
 }
